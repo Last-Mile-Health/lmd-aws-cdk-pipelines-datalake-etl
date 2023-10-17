b048676d23944ce4a395586a20229e1290684cde
Bwighane Clive Mwalwanda <bsmwalwanda@gmail.com>
2023-10-17 12:33:19 +0200

    cbmnc training transform

---
+++ lib/glue_scripts/transformation-sql/mlw_cbmnc_training.sql
@@ -0,0 +1,25 @@
+SELECT
+    start_date,
+    end_date,
+    id_number,
+    full_name,
+    phone_number,
+    health_facility,
+    catchment_area,
+    age,
+    gender,
+    position,
+    work_location,
+    highest_level_of_education,
+    district,
+    reporting_health_facility_name,
+    reporting_catchment_area_name,
+    total_score	pre_assessment_score,
+    test_type,
+    status,				
+    day,
+    month,
+    year
+FROM malawi.mlw_cbmnc_training
+GROUP BY start_date, end_date, id_number, full_name, phone_number, health_facility, catchment_area, age, gender, position, work_location, highest_level_of_education, district, reporting_health_facility_name, reporting_catchment_area_name, total_score, pre_assessment_score, test_type, status, day, month, year
+ORDER BY start_date, end_date, id_number, full_name, phone_number, health_facility, catchment_area, age, gender, position, work_location, highest_level_of_education, district, reporting_health_facility_name, reporting_catchment_area_name, total_score, pre_assessment_score, test_type, status, day, month, year
