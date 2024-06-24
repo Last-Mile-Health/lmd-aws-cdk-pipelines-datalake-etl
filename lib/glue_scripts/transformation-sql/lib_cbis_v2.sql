114a363f51dc19f09054811196aad87b38f6652a
LastMileHealth2 <113939178+LastMileHealth2@users.noreply.github.com>
2024-06-24 13:12:48 +0200

    Add files via upload

--- lib/glue_scripts/transformation-sql/lib_cbis_v2.sql
+++ lib/glue_scripts/transformation-sql/lib_cbis_v2.sql
@@ -8,5 +8,5 @@ SELECT	day,
 		organisationunitdescription,																			
 		chss_monthly_service_reports																
 FROM liberia.lib_cbis_v2
-GROUP BY day, month, year, periodname, organisationunitid, organisationunitname, organisationunitcode, organisationunitdescription, cchss_monthly_service_reports
+GROUP BY day, month, year, periodname, organisationunitid, organisationunitname, organisationunitcode, organisationunitdescription, chss_monthly_service_reports
 ORDER BY day, month, year, periodname, organisationunitid, organisationunitname, organisationunitcode, organisationunitdescription, chss_monthly_service_reports
