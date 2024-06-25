28d5cd6bfad0c34102b58fec34a0c61f7d2e0adb
LastMileHealth2 <113939178+LastMileHealth2@users.noreply.github.com>
2024-06-25 06:45:26 +0200

    Add files via upload

--- lib/glue_scripts/transformation-sql/mlw_cbmnc_training.sql
+++ lib/glue_scripts/transformation-sql/mlw_cbmnc_training.sql
@@ -1,25 +1,16 @@
-SELECT
-    start_date,
-    end_date,
-    id_number,
-    full_name,
-    phone_number,
-    health_facility,
-    catchment_area,
-    age,
-    gender,
-    position,
-    work_location,
-    highest_level_of_education,
-    district,
-    reporting_health_facility_name,
-    reporting_catchment_area_name,
-    total_score	pre_assessment_score,
-    test_type,
-    status,				
-    day,
-    month,
-    year
-FROM malawi.mlw_cbmnc_training
-GROUP BY start_date, end_date, id_number, full_name, phone_number, health_facility, catchment_area, age, gender, position, work_location, highest_level_of_education, district, reporting_health_facility_name, reporting_catchment_area_name, total_score, pre_assessment_score, test_type, status, day, month, year
-ORDER BY start_date, end_date, id_number, full_name, phone_number, health_facility, catchment_area, age, gender, position, work_location, highest_level_of_education, district, reporting_health_facility_name, reporting_catchment_area_name, total_score, pre_assessment_score, test_type, status, day, month, year
+SELECT	day,																				
+		month,																		
+		year,																																				
+		Participant Name,
+		Phonenumber,
+		Age,
+		Gender ,
+		Position,
+		Education,
+		District,
+		Health facility,
+		Catchmentarea
+																
+FROM malawi.mlw_cmbnc_training
+GROUP BY day, month, year, Participant Name, Phonenumber, Age, Gender , Position, Education, District, Health facility, Catchmentarea
+ORDER BY day, month, year, Participant Name, Phonenumber, Age, Gender , Position, Education, District, Health facility, Catchmentarea
