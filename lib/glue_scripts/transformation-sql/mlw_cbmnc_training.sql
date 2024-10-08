b177313782835c53a013403e7b0262bacf6c5d20
LastMileHealth2 <113939178+LastMileHealth2@users.noreply.github.com>
2024-10-08 14:13:40 +0200

    Update mlw_cbmnc_training.sql
    deleting total score from order by and group by

--- lib/glue_scripts/transformation-sql/mlw_cbmnc_training.sql
+++ lib/glue_scripts/transformation-sql/mlw_cbmnc_training.sql
@@ -20,5 +20,5 @@ SELECT
     month,
     year
 FROM malawi.mlw_cbmnc_training
-GROUP BY start_date, end_date, id_number, full_name, phone_number, health_facility, catchment_area, age, gender, position, work_location, highest_level_of_education, district, reporting_health_facility_name, reporting_catchment_area_name, total_score, pre_assessment_score, test_type, status, day, month, year
-ORDER BY start_date, end_date, id_number, full_name, phone_number, health_facility, catchment_area, age, gender, position, work_location, highest_level_of_education, district, reporting_health_facility_name, reporting_catchment_area_name, total_score, pre_assessment_score, test_type, status, day, month, year
+GROUP BY start_date, end_date, id_number, full_name, phone_number, health_facility, catchment_area, age, gender, position, work_location, highest_level_of_education, district, reporting_health_facility_name, reporting_catchment_area_name, pre_assessment_score, test_type, status, day, month, year
+ORDER BY start_date, end_date, id_number, full_name, phone_number, health_facility, catchment_area, age, gender, position, work_location, highest_level_of_education, district, reporting_health_facility_name, reporting_catchment_area_name, pre_assessment_score, test_type, status, day, month, year
