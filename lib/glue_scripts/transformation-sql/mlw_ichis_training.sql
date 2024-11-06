c80417f86e0283a10fd041811795590c08f828a4
LastMileHealth2 <113939178+LastMileHealth2@users.noreply.github.com>
2024-11-06 17:02:47 +0200

    Update mlw_ichis_training.sql
    removed "number" field from group by and order by conditions

--- lib/glue_scripts/transformation-sql/mlw_ichis_training.sql
+++ lib/glue_scripts/transformation-sql/mlw_ichis_training.sql
@@ -25,7 +25,7 @@ select day,
 	   family_planning_module,
 	   reporting_module
 from malawi.mlw_ichis_training
-group by day, month, year, number, district, reporting_health_facility_name, full_name_of_participant,phone_number, age, gender, training_id_number, catchment_area_population_size, years_worked_as_a_health_worker, position, education, trained_in_ichis_before, fy, quarter, community_register_module, household_register_module, person_register_module, epi_module, imci_module, eldsr_module, cbmnc_module,	family_planning_module,	reporting_module
-order by day, month, year, number, district, reporting_health_facility_name, full_name_of_participant,phone_number, age, gender, training_id_number, catchment_area_population_size, years_worked_as_a_health_worker, position, education, trained_in_ichis_before, fy, quarter, community_register_module, household_register_module, person_register_module, epi_module, imci_module, eldsr_module, cbmnc_module,	family_planning_module,	reporting_module
+group by day, month, year, district, reporting_health_facility_name, full_name_of_participant,phone_number, age, gender, training_id_number, catchment_area_population_size, years_worked_as_a_health_worker, position, education, trained_in_ichis_before, fy, quarter, community_register_module, household_register_module, person_register_module, epi_module, imci_module, eldsr_module, cbmnc_module,	family_planning_module,	reporting_module
+order by day, month, year, district, reporting_health_facility_name, full_name_of_participant,phone_number, age, gender, training_id_number, catchment_area_population_size, years_worked_as_a_health_worker, position, education, trained_in_ichis_before, fy, quarter, community_register_module, household_register_module, person_register_module, epi_module, imci_module, eldsr_module, cbmnc_module,	family_planning_module,	reporting_module
 
 
